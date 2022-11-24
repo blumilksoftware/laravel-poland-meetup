9c798ad60a310ca960acac77d566910d5479f4c7
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2022-11-24 08:51:03 +0100

    #10 Add tags and speakers to meetups list

--- frontend/components/Meetups/MeetupsTable.vue
+++ frontend/components/Meetups/MeetupsTable.vue
@@ -43,31 +43,47 @@ const filteredMeetups = computed (() => {
     return searchMeetupProperties(searchSpeakers(searchPresentations(props.meetups)))
 })
 
+const meetupSpeakers = ( meetup ) => {
+  let setSpeakers = new Set();
+
+  meetup.presentations.forEach(({ speakers }) => speakers.forEach(({ name }) => setSpeakers.add(name)))
+
+  return setSpeakers
+}
+
+const tags = (meetup) => {
+  let setTags = new Set();
+
+  meetup.presentations.forEach(({ tags }) => tags.forEach(tag => setTags.add(tag)))
+
+  return setTags
+}
+
 </script>
 
 <template>
   <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
-    <form class="my-6 -mt-5 block justify-center sm:flex">
-      <label class="relative flex w-11/12 md:w-56">
+    <form class="my-6 -mt-5 block justify-center m-auto md:w-11/12 sm:flex md:space-x-5 space-y-3 md:space-y-0">
+      <label class="relative flex md:w-11/12 md:m-none md:w-56">
         <span class="absolute inset-y-0 left-0 flex items-center pl-2">
           <magnifying-glass-icon class="mr-1.5 h-5 w-5 shrink-0 text-gray-400" aria-hidden="true"/> 
         </span>
         <span class="sr-only">Szukaj</span>
-        <input v-model="searchMeetup" type="text" class="sm:text-md block h-12 w-full rounded-sm border border-slate-300 bg-white py-2 pl-9 pr-3 text-lg text-zinc-600 shadow-sm placeholder:italic placeholder:text-zinc-400 focus:border-zinc-400 focus:outline-none focus:ring-1 focus:ring-zinc-400" placeholder="Szukaj meetupu...">
+        <input v-model="searchMeetup" type="text" class="sm:text-md block h-12 w-full rounded-xl border border-slate-300 bg-white py-2 pl-9 pr-3 text-lg text-zinc-600 shadow-sm placeholder:italic placeholder:text-zinc-400 focus:border-zinc-400 focus:outline-none focus:ring-1 focus:ring-zinc-400 hover:bg-gray-50" placeholder="Szukaj meetupu...">
       </label>
-      <label class="relative flex w-11/12 md:w-56">
+      <label class="relative flex md:w-11/12 md:m-none md:w-56">
         <span class="absolute inset-y-0 left-0 flex items-center pl-2">
           <magnifying-glass-icon class="mr-1.5 h-5 w-5 shrink-0 text-gray-400" aria-hidden="true"/> 
         </span>
         <span class="sr-only">Szukaj</span>
-        <input v-model="searchSpeaker" type="text" class="sm:text-md block h-12 w-full rounded-sm border border-slate-300 bg-white py-2 pl-9 pr-3 text-lg text-zinc-600 shadow-sm placeholder:italic placeholder:text-zinc-400 focus:border-zinc-400 focus:outline-none focus:ring-1 focus:ring-zinc-400" placeholder="Szukaj prelegentów...">
+        <input v-model="searchSpeaker" type="text" class="sm:text-md block h-12 w-full rounded-xl border border-slate-300 bg-white py-2 pl-9 pr-3 text-lg text-zinc-600 shadow-sm placeholder:italic placeholder:text-zinc-400 focus:border-zinc-400 focus:outline-none focus:ring-1 focus:ring-zinc-400 hover:bg-gray-50" placeholder="Szukaj prelegentów...">
       </label>
-      <label class="relative flex w-11/12 md:w-56">
+      <label class="relative flex md:w-11/12 md:m-none md:w-56">
         <span class="absolute inset-y-0 left-0 flex items-center pl-2">
           <magnifying-glass-icon class="mr-1.5 h-5 w-5 shrink-0 text-gray-400" aria-hidden="true"/> 
         </span>
         <span class="sr-only">Szukaj</span>
-        <input v-model="searchPresentation" type="text" class="sm:text-md block h-12 w-full rounded-sm border border-slate-300 bg-white py-2 pl-9 pr-3 text-lg text-zinc-600 shadow-sm placeholder:italic placeholder:text-zinc-400 focus:border-zinc-400 focus:outline-none focus:ring-1 focus:ring-zinc-400" placeholder="Szukaj prezentacji...">
+        <input v-model="searchPresentation" type="text" class="sm:text-md block h-12 w-full rounded-xl border border-slate-300 bg-white py-2 pl-9 pr-3 text-lg text-zinc-600 shadow-sm placeholder:italic placeholder:text-zinc-400 focus:border-zinc-400 focus:outline-none focus:ring-1 focus:ring-zinc-400 hover:bg-gray-50" placeholder="Szukaj prezentacji...">
       </label>
     </form>
     <div class="overflow-hidden bg-white">
@@ -94,11 +110,23 @@ const filteredMeetups = computed (() => {
                       </div>
                     </div>
                   </div>
+                  <div class="flex space-x-3">
+                    <p class="text-zinc-600">Prelegenci: </p>
+                    <ul class="flex space-x-1">
+                      <li v-for="speaker of meetupSpeakers(meetup)" class="flex text-zinc-600 font-semibold">{{ speaker }}{{ ',' }}</li>
+                    </ul>
+                  </div>
                   <div>
                     <ul class="list-inside  text-sm text-zinc-600">
                       <li v-for="presentation in meetup.presentations" :key="presentation.title" class="truncate py-1">
                         <chat-bubble-left-icon class="mr-1.5 inline-block h-5 w-5 shrink-0 text-gray-400"/>{{ presentation.title }}
                       </li>
+                    </ul>
+                  </div>
+                  <div class="flex space-x-3 text-zinc-500">
+                    <p>Tagi: </p>
+                    <ul class="flex space-x-1">
+                      <li v-for="tag of tags(meetup)" class="text-zinc-400">{{ tag }}{{ ',' }}</li>
                     </ul>
                   </div>
                 </div>
