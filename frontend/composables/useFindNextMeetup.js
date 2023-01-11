dc1f6a05737b3aa4ae573c2c1f1b2a4e6f0f4f38
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-11 12:59:48 +0100

    #10 cleanup

--- frontend/composables/useFindNextMeetup.js
+++ frontend/composables/useFindNextMeetup.js
@@ -5,24 +5,21 @@ export function useFindNextMeetup(meetups) {
   const today = new Date()
   const futureMeetups = []
 
-  const findMeetup = () => {
-    for(let meetup of meetups) {
-      if(new Date(meetup.date) > today) {
-        futureMeetups.push(meetup)
-      }
+  for(let meetup of meetups) {
+    if(new Date(meetup.date) > today) {
+      futureMeetups.push(meetup)
     }
-    
-    if(futureMeetups.length > 0) {
-      futureMeetups.sort((a, b) => {
-        return new Date(a.date).getTime() - new Date(b.date).getTime()
-      })
-    }
-    else if(futureMeetups == 0) 
-      return null 
-    
-    nextMeetup.value = futureMeetups[0]
   }
-  findMeetup()
   
+  if(futureMeetups.length > 0) {
+    futureMeetups.sort((a, b) => {
+      return new Date(a.date).getTime() - new Date(b.date).getTime()
+    })
+  }
+  else if(futureMeetups == 0) 
+    return null 
+  
+  nextMeetup.value = futureMeetups[0]
+
   return { nextMeetup }
 }
--- frontend/composables/useSortedMeetups.js
+++ frontend/composables/useSortedMeetups.js
@@ -4,52 +4,48 @@ export function useSortedMeetups (selectedMeetups, sortType) {
   const today = new Date()
   let sortedMeetups = []
 
-
-  function sort() {
-    for(const meetup of selectedMeetups) {
-      if(new Date(meetup.date) >= today ) {
-      futureMeetups.push(meetup)
+  for(const meetup of selectedMeetups) {
+    if(new Date(meetup.date) >= today) {
+    futureMeetups.push(meetup)
     }
-    else 
+    else {
       oldMeetups.push(meetup)
     }
-  
-    function compare( a, b ) {
-      if ( a.date < b.date ) {
-        return -1
-      }
-      if ( a.date > b.date ) {
-        return 1
-      }
-      return 0
+  }
+
+  function compare(a, b) {
+    if (a.date < b.date) {
+      return -1
     }
-    
-    futureMeetups.sort(compare)
-    oldMeetups.sort(compare).reverse()
-
-    if(sortType === 'default') {
-      for(const item of futureMeetups) {
-        sortedMeetups.push(item)
-      }
-      for(const item of oldMeetups) {
-        sortedMeetups.push(item)
-      }
-    } else if(sortType === 'newestFirst') {
-      selectedMeetups.sort(compare).reverse()
-
-      for(const item of selectedMeetups) {
-        sortedMeetups.push(item)
-      }
-    } else if(sortType === 'oldestFirst') {
-      selectedMeetups.sort(compare)
-
-      for(const item of selectedMeetups) {
-        sortedMeetups.push(item)
-      }
-    }  
+    if (a.date > b.date) {
+      return 1
+    }
+    return 0
   }
   
-  sort()
+  futureMeetups.sort(compare)
+  oldMeetups.sort(compare).reverse()
+
+  if(sortType === 'default') {
+    for(const item of futureMeetups) {
+      sortedMeetups.push(item)
+    }
+    for(const item of oldMeetups) {
+      sortedMeetups.push(item)
+    }
+  } else if(sortType === 'newestFirst') {
+    selectedMeetups.sort(compare).reverse()
+
+    for(const item of selectedMeetups) {
+      sortedMeetups.push(item)
+    }
+  } else if(sortType === 'oldestFirst') {
+    selectedMeetups.sort(compare)
+
+    for(const item of selectedMeetups) {
+      sortedMeetups.push(item)
+    }
+  }  
 
   return { sortedMeetups }
 }
