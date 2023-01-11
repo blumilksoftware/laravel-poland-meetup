1bbaf81e1a800ccc76c331adae78f725079ef344
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-11 09:06:48 +0100

    #10 Move searching next meetup logic to composable function

--- frontend/components/Landing/MeetupIndicator.vue
+++ frontend/components/Landing/MeetupIndicator.vue
@@ -1,51 +1,39 @@
 <script setup>
-import { computed, onMounted, ref } from 'vue'
+import { computed } from 'vue'
 import CountDown from './CountDown.vue'
 
-const meetupsList = ref([])
-
-onMounted(() => {
-  fetch('/api/meetups.json').then((response) => response.json()).then((meetups) => {
-    meetupsList.value = meetups
-  })
+const props = defineProps({
+  nextMeetup: {
+    type: Object,
+    default: () => {},
+  },
 })
 
-const today = new Date()
-
-const findNextMeetup = computed(() => {
-  const futureMeetups = []
-
-  for(let meetup of meetupsList.value) {
-    if(new Date(meetup.date) > today) {
-      futureMeetups.push(meetup)
-    }
-  }
-
-  futureMeetups.sort(function(a, b){
-    return (b.date) - (a.date)
-  })
-
-  return futureMeetups[futureMeetups.length - 1]
+const computedNextMeetup = computed(() => {
+  if(props.nextMeetup == 0) return {}
+  return props.nextMeetup
 })
+
+
 </script>
 
 <template>
-  <div v-if="meetupsList" class="relative bg-zinc-700">
+  <div class="relative bg-zinc-700">
     <div class="h-72 bg-zinc-600 sm:h-64 md:absolute md:left-0 md:h-full md:w-1/2">
       <img class="h-full w-full object-cover" src="/images/lpm_22/lpm22-img2.webp" alt="">
     </div>
     <div class="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-      <div v-if="findNextMeetup" class="md:ml-auto md:w-1/2 md:pl-10">
-        <count-down :date="findNextMeetup.date"/> 
-        <div v-if="meetupsList && findNextMeetup.name" class="mt-3 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-          {{ findNextMeetup.name }}
+      <div v-if="nextMeetup" class="md:ml-auto md:w-1/2 md:pl-10">
+        <count-down :date="computedNextMeetup.date"/> 
+        <div v-if="nextMeetup && computedNextMeetup.name" class="mt-3 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          {{ computedNextMeetup.name }}
         </div>
         <p class="mt-3 text-2xl leading-8 text-zinc-300">
-          <strong class="text-laravel">{{ findNextMeetup.dateFull }}</strong><br>
+          <strong class="text-laravel">{{ computedNextMeetup.dateFull }}</strong><br>
           Zapraszamy fascynatów Laravela i nie tylko!<br>
           Wstęp free.<br>
-          <span v-if="findNextMeetup.location === 'online'">Tym razem widzimy się online!</span>
-          <span v-else>Miejsce spotkania: {{ findNextMeetup.location }}</span>
+          <span v-if="computedNextMeetup.location === 'online'">Tym razem widzimy się online!</span>
+          <span v-else>Miejsce spotkania: {{ computedNextMeetup.location }}</span>
         </p>
         <div class="mt-8">
           <div class="block gap-4 rounded-md tracking-wide shadow sm:block md:block lg:w-3/5">
@@ -56,7 +44,7 @@ const findNextMeetup = computed(() => {
           </div>
         </div>
       </div>
-      <div v-else-if="!findNextMeetup" class="md:ml-auto md:w-1/2 md:pl-10">
+      <div v-else-if="!nextMeetup" class="md:ml-auto md:w-1/2 md:pl-10">
         <div class="mt-3 space-y-3 text-white sm:tracking-wider">
           <h1 class="text-3xl font-semibold sm:text-4xl">
             Już niedługo ogłosimy datę kolejnego <span class="text-laravel">meetupu!</span>
