5105f10e09a11faa64ffaf591fe750ecc42b5c1c
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-18 12:18:26 +0100

    #28 add organizers

--- frontend/components/Companies/CompaniesGrid.vue
+++ frontend/components/Companies/CompaniesGrid.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue'
+import { MapPinIcon, LightBulbIcon } from '@heroicons/vue/24/outline'
 
 const props = defineProps({
   companies: {
@@ -26,30 +27,39 @@ watch(() => props.companies, () => {
 </script>
 
 <template>
-  <div class="bg-white">
-    <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
+  <div>
+    <div class="mx-auto max-w-7xl bg-white py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
       <div class="space-y-12">
-        <h2 class="text-3xl font-bold tracking-tight text-center sm:text-4xl">
+        <h2 class="text-center text-3xl font-bold tracking-tight text-zinc-700 sm:text-4xl">
           Organizatorzy
         </h2>
-        <ul role="list" class="space-y-12 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:gap-y-12 lg:space-y-0 divide-y divide-zinc-200">
+        <ul role="list" class="space-y-12 divide-y divide-zinc-200 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:gap-y-12 lg:space-y-0 lg:divide-y-0">
           <li v-for="(company, id) in organizers" :key="id">
-            <div class="space-y-4 sm:grid sm:grid-rows-2 sm:gap-6 sm:space-y-0 lg:gap-8 divide-y-1 bg-green-100">
-              <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="relative block hover:bg-zinc-50">
-                <div class="aspect-w-3 aspect-h-1 sm:aspect-w-3 sm:aspect-h-2 mx-auto h-0 bg-red-100">
+            <div class="grid grid-cols-2 space-y-4 sm:gap-6 sm:space-y-0 lg:gap-8">
+              <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="relative">
+                <div class="aspect-w-2 aspect-h-1 sm:aspect-w-5 sm:aspect-h-2 mx-auto h-0">
                   <img class="object-contain" :src="company.logo" :alt="company.name">
                 </div>
               </router-link>
-              <div class="lg:col-span-2 bg-blue-200">
-                <div class="space-y-4">
-                  <div class="space-y-1 text-lg font-medium leading-6">
-                    <h3>{{ company.name }}</h3>
-                    <p class="text-indigo-600">
+              <div class="self-center">
+                <div class="space-y-4 text-lg font-medium leading-6">
+                  <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="flex transition duration-200 hover:translate-x-1">
+                    <light-bulb-icon class="h-6 w-6"/>
+                    <span>
+                      {{ company.name }}
+                    </span>
+                  </router-link>
+                  <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="flex text-indigo-600 transition duration-200 hover:translate-x-1">
+                    <map-pin-icon class="h-6 w-6"/>
+                    <span>
                       {{ company.location }}
-                    </p>
-                  </div>
+                    </span>
+                  </router-link>
                 </div>
               </div>
+              <p class="col-span-2 leading-6 text-zinc-700">
+                {{ company.bio }}
+              </p>
             </div>
           </li>
         </ul>
