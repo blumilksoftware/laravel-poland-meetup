356ba8b956a9c918d15610ee8661e680c6f003e5
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-23 14:36:02 +0100

    #28 add CompaniesList.vue

--- frontend/components/Companies/CompaniesGrid.vue
+++ frontend/components/Companies/CompaniesGrid.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue'
-import { MapPinIcon, LightBulbIcon, HashtagIcon } from '@heroicons/vue/24/outline'
+import CompaniesList from '@/components/Companies/CompaniesList.vue'
 
 const props = defineProps({
   companies: {
@@ -27,56 +27,8 @@ watch(() => props.companies, () => {
 </script>
 
 <template>
-  <div>
-    <div class="mx-auto max-w-7xl bg-white py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
-      <div class="space-y-12">
-        <ul role="list" class="block space-y-12 divide-y divide-zinc-200 md:flex md:space-x-12 lg:space-y-0 lg:divide-y-0">
-          <li v-for="(company, id) in organizers" :key="id">
-            <div class="grid grid-cols-2 sm:gap-6 sm:space-y-0 md:space-x-4 lg:gap-8">
-              <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="relative">
-                <div class="aspect-w-2 aspect-h-1 sm:aspect-w-5 sm:aspect-h-2 mx-auto h-0">
-                  <img class="object-contain" :src="company.logo" :alt="company.name">
-                </div>
-              </router-link>
-              <div class="self-center">
-                <div class="space-y-4 text-lg font-medium leading-6">
-                  <div class="text-laravel flex items-center text-xl">
-                    <hashtag-icon class="h-6 w-6"/>
-                    <span>organizator</span>
-                  </div>
-                  <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="flex transition duration-200 hover:translate-x-1">
-                    <light-bulb-icon class="h-6 w-6"/>
-                    <span>
-                      {{ company.name }}
-                    </span>
-                  </router-link>
-                  <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="flex text-indigo-600 transition duration-200 hover:translate-x-1">
-                    <map-pin-icon class="h-6 w-6"/>
-                    <span>
-                      {{ company.location }}
-                    </span>
-                  </router-link>
-                </div>
-              </div>
-              <p class="col-span-2 leading-6 text-zinc-700">
-                {{ company.bio }}
-              </p>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="bg-white text-center text-3xl font-bold tracking-tight text-zinc-700 sm:text-4xl">
-      <div class="mx-auto max-w-7xl bg-white py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
-        <div class="space-y-12">
-          <h2>Partnerzy</h2>
-          <ul role="list" class="space-y-12 divide-y divide-zinc-200 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:gap-y-12 lg:space-y-0 lg:divide-y-0">
-            <li v-for="(company, id) in others" :key="id">
-              {{ id }}
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+  <div class="bg-white py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
+    <companies-list :companies="organizers" name="Organizatorzy"/>
+    <companies-list :companies="others" name="Partnerzy"/>
   </div>
 </template>
---
+++ frontend/components/Companies/CompaniesList.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { HashtagIcon, MapPinIcon, LinkIcon, BuildingOffice2Icon, GlobeAmericasIcon } from '@heroicons/vue/24/outline'
+import LinkedinIcon from '@/components/Icons/LinkedinIcon.vue'
+
+const props = defineProps({
+  companies: {
+    type: Array,
+    default: () => [],
+  },
+  name: {
+    type: String,
+    default: () => [],
+  },
+})
+
+</script>
+
+<template>
+  <div class="bg-white text-center text-3xl font-bold tracking-tight text-zinc-700 sm:text-4xl">
+    <div class="mx-auto max-w-7xl bg-white py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
+      <div class="space-y-12">
+        <h2>{{ name }}</h2>
+        <ul v-auto-animate role="list" class="gap-y-12 space-y-16 divide-y divide-zinc-200 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:space-y-0 lg:divide-y-0">
+          <li v-for="(company, id) in companies" :key="id">
+            <div class="grid grid-cols-2 sm:gap-6 sm:space-y-0 md:space-x-4 lg:gap-8">
+              <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="relative">
+                <div class="self-center h-20 w-auto">
+                  <building-office-2-icon v-if="!company.logo" class="h-7 w-7 text-zinc-500" :alt="company.name"/>
+                  <img v-else class="object-contain" :src="company.logo" :alt="company.name">
+                </div>
+              </router-link>
+              <div class="self-center space-y-4 text-lg font-medium leading-6">
+                <div v-if="company.organizer" class="text-laravel flex items-center text-xl">
+                  <hashtag-icon class="h-6 w-6"/>
+                  <span>organizator</span>
+                </div>
+                <div class="space-y-4 text-lg font-medium leading-6">
+                  <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="flex space-x-2 transition duration-200 hover:translate-x-1">
+                    <link-icon class="h-6 w-6"/>
+                    <span>
+                      {{ company.name }}
+                    </span>
+                  </router-link>
+                  <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="flex space-x-2 text-indigo-600 transition duration-200 hover:translate-x-1">
+                    <map-pin-icon class="h-6 w-6"/>
+                    <span>
+                      {{ company.location }}
+                    </span>
+                  </router-link>
+                  <div class=" flex space-x-3 self-center">
+                    <a class="flex space-x-2 transition duration-200 hover:scale-110" :src="company.website">
+                      <globe-americas-icon class="h-7 w-7 text-zinc-600"/>
+                    </a>
+                    <a class="flex space-x-2 transition duration-200 hover:hover:scale-110">
+                      <linkedin-icon :href="company.linkedin" class="h-7 w-7 text-zinc-600"/>
+                    </a>
+                  </div>
+                  <p class="col-span-2 leading-6 text-zinc-700">
+                    {{ company.bio }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
