27311cb433eb7af0c95d07775e1c531d2ef2205e
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-26 08:50:56 +0100

    #28 Design of companies page

--- frontend/components/Companies/CompaniesList.vue
+++ frontend/components/Companies/CompaniesList.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue'
 import { HashtagIcon, MapPinIcon, LinkIcon, BuildingOffice2Icon, GlobeAmericasIcon } from '@heroicons/vue/24/outline'
 import LinkedinIcon from '@/components/Icons/LinkedinIcon.vue'
 
@@ -14,6 +13,7 @@ const props = defineProps({
   },
 })
 
+
 </script>
 
 <template>
@@ -24,12 +24,12 @@ const props = defineProps({
           {{ name }}
         </h2>
         <ul v-auto-animate role="list" :class="['gap-y-7 space-y-16 divide-y divide-zinc-200 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:space-y-0 lg:divide-y-0', companies.length === 1 ? 'lg:grid-cols-1' : '']">
-          <li v-for="(company, id) in companies" :key="id" class="hover:shadow-lg">
+          <li v-for="(company, id) in companies" :key="id" :class="['hover:shadow-lg', companies.length === 1 ? 'hover:shadow-none' : '']">
             <div class="my-5 ml-3 grid grid-cols-2 truncate text-base text-zinc-500 sm:gap-6 sm:space-y-0 md:space-x-4 lg:gap-8">
               <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="relative m-auto">
                 <div>
                   <building-office-2-icon v-if="!company.logo" class="h-20 w-20 text-zinc-400" :alt="company.name"/>
-                  <img v-else class="max-h-16 w-auto object-contain" :src="company.logo" :alt="company.name">
+                  <img v-else class="max-h-12 w-auto object-contain" :src="company.logo" :alt="company.name">
                 </div>
               </router-link>
               <div class="space-y-4 self-center text-lg font-medium leading-6">
@@ -44,12 +44,12 @@ const props = defineProps({
                       {{ company.name }}
                     </span>
                   </router-link>
-                  <router-link :to="{ name: 'companies.details', params: { id: company.name } }" class="flex space-x-2 text-indigo-600 transition duration-200 hover:translate-x-1">
+                  <div class="flex space-x-2 text-indigo-600 transition duration-200 hover:translate-x-1">
                     <map-pin-icon class="h-6 w-6"/>
                     <span>
                       {{ company.location }}
                     </span>
-                  </router-link>
+                  </div>
                   <div class=" flex space-x-3 self-center">
                     <a class="flex space-x-2 transition duration-200 hover:scale-110" :src="company.website">
                       <globe-americas-icon class="h-7 w-7 text-zinc-600"/>
