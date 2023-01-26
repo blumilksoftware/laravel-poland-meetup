d0314ff8340117830070998086457fbe20ed2632
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-26 15:30:56 +0100

    #28 Install mapbox, add CompaniesMap.vue

--- frontend/pages/Companies.vue
+++ frontend/pages/Companies.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue'
 import CompaniesTable from '@/components/Companies/CompaniesTable.vue'
 import CompaniesHeader from '@/components/Companies/CompaniesHeader.vue'
 import InvitationToJoin from '@/components/Companies/InvitationToJoin.vue'
+// import CompaniesMap from '@/components/Companies/CompaniesMap.vue'
 // import PageHeader from '@/components/ReusableComponents/PageHeader.vue'
 
 const companies = ref([])
@@ -19,4 +20,5 @@ onMounted(() => {
   <invitation-to-join/>
   <!-- <page-header text1="organizatorzy" text2="i" text3="partnerzy" sentence="Dołącz do nas i promoj swoją firmę!"/> -->
   <companies-table class="mt-12" :companies="companies"/>
+  <!-- <companies-map/> -->
 </template>
