4ff27d5f5eabe05692ca8c04fc5bca50bef61ea6
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-27 08:54:09 +0100

    #36 Add Company.vue

---
+++ frontend/pages/Company.vue
@@ -0,0 +1,16 @@
+<script setup>
+import PageHeader from '@/components/ReusableComponents/PageHeader.vue'
+
+let companies = ref({})
+
+onMounted (() => {
+  fetch('api/companies.json').then((response) => response.json()).then((data) => {
+  companies.value = data
+  })
+})
+
+</script>
+
+<template>
+  <page-header :text1="company.name"/>
+</template>
