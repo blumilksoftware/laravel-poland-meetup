a9233b3da78dda1b0682d218d0dc3e6035e00a9d
paulinaszkudlarek <83571355+paulinaszkudlarek@users.noreply.github.com>
2023-01-30 10:35:21 +0100

    #34 - add reusable header and background animation components (#35)
    * #34 Add PageHeader.vue and PulseBackgroundAnimation.vue
    
    * Update frontend/pages/Landing.vue
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    * Update frontend/components/ReusableComponents/PageHeader.vue
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    * #34 requested changes
    
    ---------
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>

---
+++ frontend/components/ReusableComponents/PageHeader.vue
@@ -0,0 +1,45 @@
+<script setup>
+import PulseBackgroundAnimation from '@/components/ReusableComponents/PulseBackgroundAnimation.vue'
+
+defineProps({
+  word1: {
+    type: String,
+    default: () => '',
+  },
+  word2: {
+    type: String,
+    default: () => '',
+  },
+  word3: {
+    type: String,
+    default: () => '',
+  },
+  sentence: {
+    type: String,
+    default: () => '',
+  },
+})
+
+</script>
+
+<template>
+  <div class="relative overflow-hidden bg-white">
+    <PulseBackgroundAnimation/>
+    <div class="relative py-16 sm:pb-36">
+      <main class="mx-auto max-w-7xl px-4 sm:mt-20">
+        <div class="word-center">
+          <h1 class="word-3xl word-gray-900 sm:word-5xl md:word-6xl font-bold tracking-tight">
+            <span class="block xl:inline">{{ word1 }}</span>
+            {{ ' ' }}
+            <span class="word-laravel block xl:inline">{{ word2 }}</span>
+            {{ ' ' }}
+            <span class="block xl:inline">{{ word3 }}</span>
+          </h1>
+          <blockquote class="word-base word-gray-500 sm:word-lg md:word-xl mx-auto mt-3 max-w-md md:mt-5 md:max-w-3xl">
+            {{ sentence }}
+          </blockquote>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
