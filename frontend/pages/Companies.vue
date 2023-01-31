a242c02e463c9e8839d5b4e533103ad3c18a848a
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-01-31 12:35:00 +0100

    #28 Add map to Companies.vue, add marker image

--- frontend/pages/Companies.vue
+++ frontend/pages/Companies.vue
@@ -3,22 +3,23 @@ import { onMounted, ref } from 'vue'
 import CompaniesTable from '@/components/Companies/CompaniesTable.vue'
 import CompaniesHeader from '@/components/Companies/CompaniesHeader.vue'
 import InvitationToJoin from '@/components/Companies/InvitationToJoin.vue'
-// import CompaniesMap from '@/components/Companies/CompaniesMap.vue'
+import CompaniesMap from '@/components/Companies/Map/CompaniesMap.vue'
 // import PageHeader from '@/components/ReusableComponents/PageHeader.vue'
 
 const companies = ref([])
 
-onMounted(() => {
-  fetch('/api/companies.json').then((response) => response.json()).then((data) => {
+onMounted(async () => {
+  await fetch('/api/companies.json').then((response) => response.json()).then((data) => {
     companies.value = data
   })
 })
+
 </script>
 
 <template>
   <companies-header/>
-  <!-- <page-header text1="organizatorzy" text2="i" text3="partnerzy" sentence="Dołącz do nas i promoj swoją firmę!"/> -->
+  <companies-map :companies="companies"/>
+  <!-- <page-header word1="organizatorzy" word2="i" word3="partnerzy" sentence="Dołącz do nas i promoj swoją firmę!"/> -->
   <companies-table class="mt-12" :companies="companies"/>
-  <!-- <companies-map/> -->
   <invitation-to-join/>
 </template>
