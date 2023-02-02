5848b70dfc55eb8705d8751e3246f2df0c91d1cc
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-02 13:08:21 +0100

    #36 Add Create CompanyHeader.vue

---
+++ frontend/components/Company/CompanyHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  company: {
+    type: Object,
+    default: () => {},
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+})
+
+console.log('company', props.company)
+</script>
+<template>
+  <div v-if="company" class="w-full">
+    <div class="h-20 w-full">
+      {{ company.name }}
+      <img :src="company.logo" :alt="'logo'+ company.name"> 
+    </div>
+  </div>
+</template>
