dbb351a25e1d51d14b229d251b2fe129a0ceb842
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-09 10:34:50 +0100

    #36 Add ListTabs.vue and useBuildList.js - building meetups, presentations and speakers lists  company site

---
+++ frontend/composables/useBuildLists.js
@@ -0,0 +1,45 @@
+import { ref, computed } from 'vue'
+
+export function useBuildLists(meetupsList, companyName) {
+  const meetups = ref(new Set())
+  const presentations = ref(new Set()) 
+  const speakers = ref(new Set())
+  
+  
+  const lists = computed (() => { 
+    return {
+      meetups: {
+        id: 'meetups',
+        name: 'Meetupy', 
+        data: meetups.value,
+      },
+      presentations: {
+        id: 'presentations',
+        name: 'Prezentacje', 
+        data: presentations.value,
+      },
+      speakers: {
+        id: 'speakers',
+        name: 'Prelegenci', 
+        data: speakers.value,
+      },
+    }
+  })
+
+  const buildLists = () => {
+    return meetupsList.filter(function(meetup) {
+      meetup.presentations.filter(function(presentation) {
+        presentation.speakers.forEach(speaker => {
+          if (speaker.company === companyName) {
+            meetups.value.add(meetup)
+            presentations.value.add(presentation)
+            speakers.value.add(speaker)
+          }
+        })
+      })
+    })
+  }
+  buildLists()
+
+  return { lists } 
+}
