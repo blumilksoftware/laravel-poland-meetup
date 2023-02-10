a40d052f14880c028690460d3b9797da1740bd2a
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-10 14:22:44 +0100

    #36 Design CompanyDetails.vue

--- frontend/components/Company/CompanyDetails.vue
+++ frontend/components/Company/CompanyDetails.vue
@@ -1,10 +1,8 @@
 <script setup>
-import ListTabs from '@/components/Company/ListTabs.vue'
-
-const props = defineProps({
+defineProps({
   company: {
     type: Object,
-    default: () => {},
+    default: () => { },
   },
   meetups: {
     type: Array,
@@ -18,9 +16,22 @@ const props = defineProps({
 
 </script>
 <template>
-  <div class="my-12 bg-white text-center text-3xl font-bold tracking-tight text-zinc-700 shadow-lg sm:text-xl">
-    <div class="mx-auto block w-full max-w-7xl py-12 px-4 sm:flex sm:px-6 lg:px-8 lg:py-24">
-      <list-tabs :name="name" :meetups="meetups"/>
+  <div class="my-10 bg-white tracking-tight text-zinc-700 shadow-lg">
+    <div v-if="company.bio" class="lg:py-18 mx-auto max-w-7xl p-4 sm:px-6 lg:px-8">
+      <div class="space-y-12">
+        <h2 class="first-letter:text-laravel lg:py-18 w-full justify-center border-b py-10 px-7 text-2xl font-bold sm:px-6 md:text-4xl lg:px-8">
+          O firmie
+        </h2>
+        <div class="h-full text-xl md:flex md:flex-row-reverse md:text-3xl">
+          <div class="my-10 w-full self-center md:w-1/3">
+            <img class="mx-auto h-14 md:h-20" :src="company.logo">
+          </div>
+          <div class="text-md mx-4 my-7 text-center italic leading-8 text-zinc-600 md:w-2/3 md:text-lg md:font-normal md:tracking-wide lg:font-semibold">
+            {{ company.bio }}
+          </div>
+        </div>
+        <div class="my-5 w-full"/>
+      </div>
     </div>
   </div>
 </template>
