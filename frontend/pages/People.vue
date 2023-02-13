5a0a17a6dbf614e8fa41a078ac22ea3d2b386e5b
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-13 12:38:17 +0100

    #40 Add People.vue

---
+++ frontend/pages/People.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import PeopleTable from '@/components/People/PeopleTable.vue'
+
+const people = ref([])
+
+onMounted(() => {
+  async function fetchPeople () {
+    const response = await fetch('/api/people.json')
+    people.value = await response.json()
+    return people
+  }
+  fetchPeople().then(people => {
+    people
+  })
+})
+
+</script>
+
+<template>
+  <people-table class="mt-12" :speakers="people"/>
+</template>
