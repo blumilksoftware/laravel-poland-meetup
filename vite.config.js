661d212d4d28c07bcf6fd81709cbefcb681763c3
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2023-02-15 09:22:37 +0100

    #20 - boolean casting

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return {
-    base: process.env.VITE_GITHUB_ALIAS_ENABLED
+    base: process.env.VITE_GITHUB_ALIAS_ENABLED === "true"
       ? '/laravel-poland-meetup/'
       : '/',
     resolve: {
