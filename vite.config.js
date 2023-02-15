7ea6a53b81445950418ddcc5f783c7edb7a1d42b
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2023-02-15 09:26:44 +0100

    #20 - deploy to GH Pages (#44)
    * #20 - deploy to GH Pages
    
    * #20 - out -> dist
    
    * #20 - boolean casting

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': '/frontend',
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return {
+    base: process.env.VITE_GITHUB_ALIAS_ENABLED === "true"
+      ? '/laravel-poland-meetup/'
+      : '/',
+    resolve: {
+      alias: {
+        '@': '/frontend',
+      },
     },
-  },
-  plugins: [vue()],
-  server: {
-    watch: {
-      usePolling: true,
+    plugins: [vue()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      port: 9088,
     },
-    port: 9088,
-  },
+  }
 })
