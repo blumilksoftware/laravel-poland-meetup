624bb332b4b1276ce027c120438defb5894d5160
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2023-02-15 10:10:07 +0100

    #20 - deploy to GH Pages tweaks

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
-  console.log(process.env.VITE_GITHUB_ALIAS_ENABLED)
-  console.log(process.env.VITE_GITHUB_ALIAS_ENABLED === "true")
-  console.log(process.env.VITE_GITHUB_ALIAS_ENABLED === "true"
-    ? '/laravel-poland-meetup/'
-    : '/')
-
-  return {
-    base: process.env.VITE_GITHUB_ALIAS_ENABLED === "true"
-      ? '/laravel-poland-meetup/'
-      : '/',
-    resolve: {
-      alias: {
-        '@': '/frontend',
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/frontend',
     },
-    plugins: [vue()],
-    server: {
-      watch: {
-        usePolling: true,
-      },
-      port: 9088,
+  },
+  plugins: [vue()],
+  server: {
+    watch: {
+      usePolling: true,
     },
-  }
+    port: 9088,
+  },
 })
