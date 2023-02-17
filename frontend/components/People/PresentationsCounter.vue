95325dd48760deb9f1b4c113fedbc8106b91a1a1
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-17 08:10:19 +0100

    #40 Rebuild PresentationsCounter.vue

--- frontend/components/People/PresentationsCounter.vue
+++ frontend/components/People/PresentationsCounter.vue
@@ -1,26 +1,44 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const props = defineProps({
   meetups: {
     type: Array, 
     default: () => [],
   },
-  speakerName: {
-    type: String,
-    default: '',
+  speaker: {
+    type: Object,
+    default: () => {},
   },
 })
 
 const countPresentations = ref(0)
-props.meetups.filter(function(meetup) {
-  meetup.presentations.filter(function(presentation) {
-    presentation.speakers.filter(function(speaker) {
-      if(speaker.name === props.speakerName) {
-        countPresentations.value++}
+
+function count () { 
+  return props.meetups.filter(function(meetup) {
+    meetup.presentations.filter(function(presentation) {
+      presentation.speakers.filter(function(speaker) {
+        if(speaker.name === props.speaker.name) {
+          countPresentations.value++
+        }
+      })
     })
   })
+}
+
+watch(() => props.meetups, () => {
+  count()
 })
+
+const emit = defineEmits(['counted'])
+
+watch(countPresentations, () => {
+  emit('counted', { 
+    ...props.speaker,
+    countedPresentations: countPresentations.value,
+  })
+})
+
 </script>
 <template>
   <div>{{ countPresentations }}</div>
