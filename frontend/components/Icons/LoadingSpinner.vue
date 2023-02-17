991c8eb2154d6dcc64df0593e2d50deb2e7718f4
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-17 12:38:05 +0100

    Add LoadingSpinner.vue

---
+++ frontend/components/Icons/LoadingSpinner.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="loadingio-spinner-spinner-8ug9vg1oq0h">
+    <div class="ldio-5mh5u5buhot mt-56">
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+    </div>
+  </div>
+</template>
+<style scoped>
+@keyframes ldio-5mh5u5buhot {
+  0% {
+    opacity: 1
+  }
+
+  100% {
+    opacity: 0
+  }
+}
+
+.ldio-5mh5u5buhot div {
+  left: 94px;
+  top: 48px;
+  position: absolute;
+  animation: ldio-5mh5u5buhot linear 1s infinite;
+  background: #a1a1aa;
+  width: 12px;
+  height: 24px;
+  border-radius: 6px / 12px;
+  transform-origin: 6px 52px;
+}
+
+.ldio-5mh5u5buhot div:nth-child(1) {
+  transform: rotate(0deg);
+  animation-delay: -0.9166666666666666s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(2) {
+  transform: rotate(30deg);
+  animation-delay: -0.8333333333333334s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(3) {
+  transform: rotate(60deg);
+  animation-delay: -0.75s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(4) {
+  transform: rotate(90deg);
+  animation-delay: -0.6666666666666666s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(5) {
+  transform: rotate(120deg);
+  animation-delay: -0.5833333333333334s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(6) {
+  transform: rotate(150deg);
+  animation-delay: -0.5s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(7) {
+  transform: rotate(180deg);
+  animation-delay: -0.4166666666666667s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(8) {
+  transform: rotate(210deg);
+  animation-delay: -0.3333333333333333s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(9) {
+  transform: rotate(240deg);
+  animation-delay: -0.25s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(10) {
+  transform: rotate(270deg);
+  animation-delay: -0.16666666666666666s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(11) {
+  transform: rotate(300deg);
+  animation-delay: -0.08333333333333333s;
+  background: #a1a1aa;
+}
+
+.ldio-5mh5u5buhot div:nth-child(12) {
+  transform: rotate(330deg);
+  animation-delay: 0s;
+  background: #a1a1aa;
+}
+
+.loadingio-spinner-spinner-8ug9vg1oq0h {
+  width: 200px;
+  height: 100vh;
+  display: inline-block;
+  overflow: hidden;
+  background: transparent;
+}
+
+.ldio-5mh5u5buhot {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform: translateZ(0) scale(1);
+  backface-visibility: hidden;
+  transform-origin: 0 0;
+}
+
+.ldio-5mh5u5buhot div {
+  box-sizing: content-box;
+}
+</style>
