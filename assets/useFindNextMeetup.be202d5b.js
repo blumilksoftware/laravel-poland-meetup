e75d3022f2dcd1b5a4cf0eb87b13de0ae2809f15
paulinaszkudlarek <83571355+paulinaszkudlarek@users.noreply.github.com>
2023-02-20 12:20:45 +0000

    Deploying to public from @ blumilksoftware/laravel-poland-meetup@8a1e8114889580afd6939f192ee0ab5be2bc5e0f 🚀

--- assets/useFindNextMeetup.be202d5b.js
+++
@@ -1,1 +0,0 @@
-import{j as u}from"./index.c6e19dbd.js";const p="/images/speakers/placeholder.webp";function i(n){let a=u(null);const r=new Date,e=[];for(let t of n)new Date(t.date)>r&&e.push(t);return e.length>0&&(e.sort((t,s)=>new Date(t.date).getTime()-new Date(s.date).getTime()),a.value=e[0]),{nextMeetup:a}}export{p as _,i as u};
