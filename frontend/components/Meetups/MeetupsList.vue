5ed57ad9b6a7a345a3bf07c290e0f25208c08d58
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-20 09:25:03 +0100

    #48 Design empty states

--- frontend/components/Meetups/MeetupsList.vue
+++ frontend/components/Meetups/MeetupsList.vue
@@ -116,23 +116,6 @@ const meetupTags = ( meetup ) => {
     </li>
     <li v-if="(!data.length && !loading && !error)" class="space-y-7 text-xl text-zinc-500">
       <NoResults/>
-      <!-- <p class="m-8 text-left md:text-center">
-        Nie znaleźliśmy pasujących wyników.
-      </p>
-      <ul class="m-9 w-fit list-disc md:m-auto md:text-left">
-        <li>
-          Sprawdź, czy szukana fraza nie zawiera błędów.
-        </li>
-        <li>
-          Spróbuj użyć innych słów kluczowych.
-        </li>
-        <li>
-          Spróbuj użyć bardziej ogólnych słów kluczowych.
-        </li>
-      </ul>
-      <div class="block justify-center md:m-auto md:flex md:w-3/5">
-        <img class="center h-50 mx-auto" src="/images/placeholders/noresults.webp" alt="brak wyników wyszukiwania">
-      </div> -->
     </li>
   </ul>
 </template>
