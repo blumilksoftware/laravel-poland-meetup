d25fb1e08416954049f60b4c34ff4164b6adcb69
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-02-27 12:07:41 +0100

    Find speaker using slug

--- frontend/pages/Speaker.vue
+++ frontend/pages/Speaker.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-// import { useRoute } from 'vue-router'
+import { useRoute } from 'vue-router'
 import PageHeader from '@/components/ReusableComponents/PageHeader.vue'
 import SpeakerTable from '@/components/Speaker/SpeakerTable.vue'
 import NoDataError from '@/components/EmptyStates/NoDataError.vue'
@@ -8,17 +8,17 @@ import LoadingSpinner from '@/components/Icons/LoadingSpinner.vue'
 
 const meetups = ref([])
 const people = ref([])
-// const route = useRoute()
+const route = useRoute()
 let loading = ref(true)
 let error = ref(false)
-// const speaker = ref({})
-// const slug = route.params.id
+const speaker = ref({})
+const slug = route.params.id
 
-// for(let item of people.value) {
-//   if(item.slug === slug) {
-//     speaker.value = item
-//   }
-// }
+    for(let item of people.value) {
+      if(item.slug === slug) {
+        speaker.value = item
+      }
+    }
 
 
 onMounted(() => {
@@ -53,7 +53,7 @@ onMounted(() => {
   <NoDataError :error="error" text="Brak prelegenta"/>
   <LoadingSpinner v-if="loading"/>
   <div v-if="!error && !loading">
-    <PageHeader word1="Imię"/>
+    <PageHeader word1="Marek Tenus"/>
     <SpeakerTable :loading="loading" :meetups="meetups" :speaker="speaker"/>
   </div>
 </template>
