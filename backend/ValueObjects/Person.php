f7c9701c0a9bedab3f2827b03811b3bf8fe9e760
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-03-02 11:01:34 +0100

    Add slug in builder.php

--- backend/ValueObjects/Person.php
+++ backend/ValueObjects/Person.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace LaravelPolandMeetup\ValueObjects;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class Person
 {
+    public readonly string $slug;
+
     public function __construct(
         public readonly string $id,
         public readonly string $name,
@@ -15,7 +18,10 @@ class Person
         public readonly ?string $bio = null,
         public readonly ?string $linkedin = null,
         public readonly Collection $meetups = new Collection(),
-    ) {}
+    ) {
+        $this->slug = Str::slug($this->name);
+    }
+
 
     public function toListedEntry(): array
     {
@@ -24,6 +30,7 @@ class Person
             "name" => $this->name,
             "image" => $this->image,
             "bio" => $this->bio,
+            "slug" => $this->slug,
             "linkedin" => $this->linkedin,
             "avatar" => "https://ui-avatars.com/api/?background=random&font-size=.33&size-128&rounded=true&name=" . str_replace(" ", "-", $this->name),
         ];
@@ -36,6 +43,7 @@ class Person
             "name" => $this->name,
             "image" => $this->image,
             "bio" => $this->bio,
+            "slug" => $this->slug,
             "linkedin" => $this->linkedin,
             "avatar" => "https://ui-avatars.com/api/?background=random&font-size=.33&size-128&rounded=true&name=" . str_replace(" ", "-", $this->name),
         ];
