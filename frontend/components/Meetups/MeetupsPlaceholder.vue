78ae4f334c6b8c91934e5d15cf5959fdb79c53f5
paulinaszkudlarek <83571355+paulinaszkudlarek@users.noreply.github.com>
2023-03-03 11:16:09 +0100

    #42 - Lazy loaded data in meetups lists (#58)
    * Add
    
    * Change name MeetupsListSkeleton to MeetupsPlaceholder

---
+++ frontend/components/Meetups/MeetupsPlaceholder.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="relative block animate-pulse divide-y divide-zinc-200 bg-white transition duration-300 hover:bg-zinc-50">
+    <li v-for="(i, id) of 5" :key="id" class="flex items-center">
+      <div class="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+        <div class="w-full space-y-2 p-4 sm:px-6">
+          <div class="flex space-x-4">
+            <p class="h-7 w-64 bg-zinc-200"/>
+          </div>
+          <div class="my-3 flex items-center gap-4 ">
+            <p class="h-4 w-56 bg-zinc-50"/>
+          </div>
+          <div>
+            <ul class="my-4 list-inside">
+              <li class="my-3 h-5 w-72 bg-zinc-100 py-1"/>
+              <li class="my-3 h-5 w-80 bg-zinc-100 py-1"/>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="hidden md:flex">
+        <ul class="mr-10 flex -space-x-2 overflow-hidden">
+          <li class="my-2 inline-block h-12 w-12 rounded-full bg-zinc-100 shadow-md"/>
+          <li class="my-2 inline-block h-12 w-12 rounded-full bg-zinc-100 shadow-md"/>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</template>
