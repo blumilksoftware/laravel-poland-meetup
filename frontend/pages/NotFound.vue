44331a68b907064ce95a1c324ceb091d248d07a9
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-03-09 12:33:55 +0100

    Design of NotFound.vue

--- frontend/pages/NotFound.vue
+++ frontend/pages/NotFound.vue
@@ -1,19 +1,27 @@
+<script setup>
+import { ArrowUturnLeftIcon, ExclamationCircleIcon } from '@heroicons/vue/24/outline'
+</script>
+
 <template>
   <div class="mx-auto h-full w-full max-w-7xl bg-white">
     <div class="mx-auto max-w-4xl">
       <p class="my-8 pt-10 text-center text-xl sm:my-12 sm:text-2xl">
         Strona, której szukasz nie istnieje.
       </p>
-      <ul class="list-disc">
-        <li class="text-md mx-8 my-5 text-left sm:my-6 sm:mx-20 sm:text-xl">
-          Spróbuj odświeżyć stronę.
-        </li>
-        <li class="text-md mx-8 my-5 text-left sm:my-6 sm:mx-20 sm:text-xl">
-          <router-link :to="{ name: 'landing' }" class="underline hover:bg-zinc-50">
-            Wróć na stronę główną.
-          </router-link>
-        </li>
-      </ul>
+      <div>
+        <div class="flex items-center space-x-3">
+          <exclamation-circle-icon class="h-6 w-6"/>
+          <p class="text-md mx-8 text-left hover:underline sm:my-3 sm:mx-20 sm:text-xl">
+            Wprowadź inny adres url lub...
+          </p>
+        </div>
+        <router-link :to="{ name: 'landing' }" class="flex items-center space-x-3">
+          <arrow-uturn-left-icon class="h-5 w-5"/>
+          <p class="text-md underline mx-8 text-left hover:underline sm:my-3 sm:mx-20 sm:text-xl">
+            Wróć na stronę główną
+          </p>
+        </router-link>
+      </div>
     </div>
     <img src="/images/placeholders/error.webp" class="mx-auto lg:w-2/3">
   </div>
