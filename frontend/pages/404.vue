314d9476bda0fde4ae9d26ae705d1c08748e07b1
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-03-10 13:14:03 +0100

    Small changes in 404.vue design

--- frontend/pages/404.vue
+++ frontend/pages/404.vue
@@ -5,10 +5,10 @@ import { ArrowUturnLeftIcon, ExclamationCircleIcon } from '@heroicons/vue/24/out
 <template>
   <div class="mx-auto h-full w-full max-w-7xl bg-white">
     <div class="mx-auto max-w-4xl">
-      <p class="my-8 pt-10 text-center text-xl sm:my-12 sm:text-2xl">
+      <p class="my-8 pt-10 text-center text-lg sm:my-12 sm:text-2xl">
         Strona, której szukasz nie istnieje.
       </p>
-      <div>
+      <div class="mx-5 space-y-3">
         <div class="flex items-center space-x-3">
           <exclamation-circle-icon class="h-6 w-6"/>
           <p class="text-md mx-8 text-left sm:my-3 sm:mx-20 sm:text-xl">
