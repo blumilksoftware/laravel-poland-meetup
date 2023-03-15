35fb27dfcfb3ca785326f8ec406292793c066667
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-03-15 09:35:11 +0100

    Add Google icon

--- frontend/components/Contact/ContactForm.vue
+++ frontend/components/Contact/ContactForm.vue
@@ -1,5 +1,6 @@
 <script setup>
 import PulseBackgroundAnimation from '@/components/ReusableComponents/PulseBackgroundAnimation.vue'
+import GoogleIcon from '@/components/Icons/GoogleIcon.vue'
 
 let url = import.meta.env['VITE_CONTACT_FORM_URL'] + '/formResponse'
 let emailName = import.meta.env.VITE_CONTACT_FORM_EMAIL_NAME
@@ -17,7 +18,7 @@ function handleFormSubmit () {
   <div class="relative my-12 overflow-hidden bg-white tracking-tight text-zinc-700 shadow-lg">
     <PulseBackgroundAnimation/>
     <div class="relative mx-auto max-w-7xl p-4 sm:px-6 lg:py-24 lg:px-8">
-      <div class="block space-y-12 md:flex">
+      <div class="block space-y-12 md:mx-12 md:flex">
         <div class="block md:w-1/2">
           <div class="my-12 space-y-6">
             <h2 class="text-laravel text-xl font-semibold lg:text-3xl">
@@ -30,22 +31,19 @@ function handleFormSubmit () {
               Jeśli chcesz dołączyć do nas i współtworzyć <strong>Laravel <span class="text-laravel">Poland</span> Meetup</strong> i promować swoją firmę, to daj nam znać - odezwiemy się. 
             </p>
           </div>
-          <form id="contact"  onsubmit="" class="relative z-10 text-base" method="POST" target="_blank" :action="url">
+          <form id="contact" onsubmit="" class="relative z-10 text-base" method="POST" target="_blank" :action="url">
             <div class="space-y-4 py-4 md:px-4">
               <div>
                 <label :for="emailName" class="block text-left text-gray-700">Email</label>
-                <input :id="emailName" type="text" :name="emailName" placeholder="example@example.com" class="my-1 block w-full rounded-md border-gray-300 px-4 py-2 shadow-sm">
+                <input :id="emailName" type="email" :name="emailName" placeholder="example@example.com" class="my-1 block w-full rounded-md border-gray-300 px-4 py-2 shadow-sm">
               </div>
               <div>
                 <label :for="messageName" class="block text-left text-gray-700">Wiadomość</label>
-                <textarea :id="messageName" rows="8" :name="messageName"
-                          placeholder="Zadaj nam pytanie"
-                          class="mt-1 block w-full rounded-md border-gray-300 px-4 py-2 shadow-sm"
-                />
+                <textarea :id="messageName" rows="8" :name="messageName" placeholder="Zadaj nam pytanie" class="mt-1 block w-full rounded-md border-gray-300 px-4 py-2 shadow-sm"/>
               </div>
               <div class="md:flex md:justify-end">
-                <button :click="handleFormSubmit" class="mt-1 cursor-pointer rounded-md bg-zinc-200 px-4 py-2 text-center text-zinc-700">
-                  Wyślij przez Google Forms
+                <button :click="handleFormSubmit" class="mt-1 flex cursor-pointer rounded-md bg-zinc-200 px-4 py-2 text-center text-zinc-700">
+                  Wyślij przez <GoogleIcon class="mx-1 flex h-5 w-5 self-center"/> Forms
                 </button>
               </div>
             </div>
