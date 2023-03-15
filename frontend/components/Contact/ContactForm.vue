62122360cb4c63b29c1fcbef6f61ffba488e04c3
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-03-15 12:04:54 +0100

    Setence change

--- frontend/components/Contact/ContactForm.vue
+++ frontend/components/Contact/ContactForm.vue
@@ -25,10 +25,10 @@ function handleFormSubmit () {
               Napisz do nas
             </h2>
             <p class="text-base md:text-lg">
-              Masz pytanie? Napisz, a chętnie Ci odpowiemy!
+              Masz pytanie? Chętnie Ci na nie odpowiemy!
             </p>
             <p class="text-base md:text-lg">
-              Jeśli chcesz dołączyć do nas i współtworzyć <strong>Laravel <span class="text-laravel">Poland</span> Meetup</strong> i promować swoją firmę, to daj nam znać - odezwiemy się. 
+              Jeśli chcesz dołączyć do nas, współtworzyć <strong>Laravel <span class="text-laravel">Poland</span> Meetup</strong> i promować swoją firmę, to daj nam znać - skontaktujemy się z Tobą. 
             </p>
           </div>
           <form id="contact" onsubmit="" class="relative z-10 text-base" method="POST" target="_blank" :action="url">
@@ -42,7 +42,7 @@ function handleFormSubmit () {
                 <textarea :id="messageName" rows="8" :name="messageName" placeholder="Zadaj nam pytanie" class="mt-1 block w-full rounded-md border-zinc-300 px-4 py-2 shadow-sm focus:border-zinc-500 focus:ring-zinc-300"/>
               </div>
               <div class="md:flex md:justify-end">
-                <button :click="handleFormSubmit" class="mt-1 flex cursor-pointer rounded-md border-zinc-300 bg-zinc-200 px-4 py-2 text-center text-zinc-700 transition-transform focus:border-zinc-500  focus:outline-none focus:ring-zinc-300 active:scale-75">
+                <button :click="handleFormSubmit" class="mt-1 flex cursor-pointer rounded-md border-zinc-300 bg-zinc-200 px-4 py-2 text-center text-zinc-700 transition-transform focus:border-zinc-500 focus:ring-zinc-300 active:scale-75">
                   Wyślij przez <GoogleIcon class="mx-1 flex h-5 w-5 self-center"/> Forms
                 </button>
               </div>
