7188fedf316974f3bc4c22118c194bea19a18822
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2023-03-20 10:14:40 +0100

    RWd fixes

--- frontend/components/Meetups/FilterCategory.vue
+++ frontend/components/Meetups/FilterCategory.vue
@@ -47,15 +47,15 @@ watch(selectedElements, () => {
     <transition enter-active-class="transition ease-in-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
       <ComboboxOptions static class="my-3 block w-full divide-y divide-zinc-100 overflow-auto rounded-md">
         <ComboboxOption v-for="item in filteredElements" v-slot="{ active, selected }" :key="item.name" :value="item.name" as="div">
-          <li :class="['relative cursor-default select-none', active ? 'bg-zinc-100 text-zinc-700' : 'text-zinc-700', selected ? 'bg-zinc-200' : '']">
-            <div class="mx-3 flex items-center py-3">
+          <li :class="['flex justify-between cursor-default select-none', active ? 'bg-zinc-100 text-zinc-700' : 'text-zinc-700', selected ? 'bg-zinc-200' : '']">
+            <div class="ml-3 mr-1 flex items-center py-3">
               <img v-if="item.image && !item.image.length" src="/images/speakers/placeholder.webp" alt="" class="h-6 w-6 shrink-0 rounded-full">
               <img v-else-if="item.image" :src="item.image" alt="" class="h-6 w-6 shrink-0 rounded-full">
               <span :class="['ml-3 truncate text-base text-zinc-500', selected && 'font-semibold']">
                 {{ item.name }}
               </span>
             </div>
-            <span v-if="selected" :class="['absolute inset-y-0 right-0 flex items-center pr-4 hover:text-red-200 mr-6', active ? 'text-zinc-700' : 'text-zinc-500']">
+            <span v-if="selected" :class="['flex items-center hover:text-red-200 mr-1 sm:mr-4', active ? 'text-zinc-700' : 'text-zinc-500']">
               <check-icon class="h-5 w-5" aria-hidden="true"/>
             </span>
           </li>
