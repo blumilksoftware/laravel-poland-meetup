4d9529d2f27c30738e4c1f95db456dc53c73333e
EwelinaSkrzypacz <56546832+EwelinaSkrzypacz@users.noreply.github.com>
2023-10-09 11:16:25 +0000

    Deploying to public from @ blumilksoftware/laravel-poland-meetup@9575d7233ae3b15f7980172e7c6ddb7e630f8501 🚀

--- assets/useFindNextMeetup.3ef48288.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index.46eb899e.js";const p="/images/speakers/placeholder.webp";function i(n){let a=o({});const r=new Date,e=[];for(let t of n)new Date(t.date)>r&&e.push(t);return e.length>0&&(e.sort((t,s)=>new Date(t.date).getTime()-new Date(s.date).getTime()),a.value=e[0]),{nextMeetup:a}}export{p as _,i as u};
